11416d6f351039318aa2e98f1d7f752c3eeec7f5
Niko Hienonen <niko.hienonen@gmail.com>
2019-02-06 11:10:12 +0200

    are

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-
 function readyUp(){
-    function MyFetch(url, id, element) {    //'Object' to do the fetching
+    //'Object' to do the fetching
+    function MyFetch(url, id, element) {
+        this.url = url;                 //Assign the url 
+        this.id = id;                   //Assign the id
+        this.element = element;         //Assign the img-element to the function    
 
-        this.url = url;                 //assign the url 
-        this.id = id;
-        this.element = element;                   //assign the img-element to the function    
-
-        //fetch the content of the url and send the acquired J-son object to the assigned function
+        //Fetch the content of the url and send the acquired J-son object to the assigned function
         this.fetchTheUrl = async() => {
             this.url = this.url+this.id
 
             // Create a GET request for the character with url https://rickandmortyapi.com/api/character/randomIdWhichWasGenerated
-            // Instead of continuing to the next line, wait for the request to finish (await).
+            // Instead of continuing to the next line, wait for the request to finish (await)
             const response = await fetch(this.url);
 
-            // Get the JSON of the response, wait for the request to complete and pass it to "result" variable.
+            // Get the JSON of the response, wait for the request to complete and pass it to "result" variable
             const result = await response.json();
 
             // Call the done-function and give result as a parameter to it.
@@ -31,6 +30,7 @@ function readyUp(){
             element.src = result.image;
             element.name = result.name;
         }
+        //Set the answer and and add a listener to it
         function picked(){
             let correctAnswer = document.querySelector('.correctAnswer');
             let correct = correctAnswer.getAttribute("name")
@@ -50,17 +50,19 @@ function readyUp(){
     function getRndInteger(min, max) {
         return Math.floor(Math.random() * (max - min + 1) ) + min;
     }
+    //Randomly select one of the images to be the right answer
     function setRightAnswer(imgs){
         let x = getRndInteger(0, 3); 
         let correct = imgs[x];
         correct.classList.add('correctAnswer'); 
     }
+    //Print out the question to the player
     function askTheQuestion(){
         let correct = document.querySelector('.correctAnswer').getAttribute("name");
         let question = document.querySelector('.word');
         question.innerHTML = correct+"?";
     }
-
+    //Check if the id has already been taken
     function checkID(id){
         let ret = 1;
         ids.forEach(element => {
@@ -70,6 +72,7 @@ function readyUp(){
         });
         return ret
     }
+    //Get one of the IDs from the API
     function getID(){
         let id = getRndInteger(1, 493);
         let ret = 303;
@@ -80,9 +83,9 @@ function readyUp(){
         }
         return ret;
     }
-    //retrieve the images(4)
+    //Retrieve the images(4) and push them to the array
     let imgs = document.querySelectorAll('.charImg');
-    let ids = [""];
+    let ids = [];
 
     imgs.forEach(element => {
         let id = getID();
@@ -92,9 +95,9 @@ function readyUp(){
     });
     setTimeout(()=>{
         setRightAnswer(imgs);
-        askTheQuestion()}, 1100)
+        askTheQuestion()}, 500)
     
 }
 
-//Wait untill the window has loaded
+//Wait until the window has loaded
 window.addEventListener('load', readyUp);
